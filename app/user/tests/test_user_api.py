33b1dbae9acd0707a9368bb295ca3e3229a26d2d
Allan <allanjs88@gmail.com>
2022-04-25 08:48:41 -0600

    Added create tag feature.

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -66,8 +66,6 @@ class PublicUserApiTests(TestCase):
         create_user(**payload)
 
         res = self.client.post(TOKEN_URL, payload)
-        print('res.data')
-        print(res.data)
         self.assertIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
 
